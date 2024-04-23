e066a89793ded7f277094effaaf18e5ebcd47e25
Forge <forgeinnovation.main@gmail.com>
2024-04-23 21:25:41 +0530

    ssl

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --no-input
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 6
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
